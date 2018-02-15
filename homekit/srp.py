43c320f9ac7b4beb647f79c7b0ac33ed83247776
Mike Stegeman <mrstegeman@gmail.com>
2018-02-15 06:48:31 -0700

    Remove gmpy2 requirement.

--- homekit/srp.py
+++ homekit/srp.py
@@ -5,7 +5,6 @@ https://tools.ietf.org/html/rfc5054. See HomeKit spec page 36 for adjustments im
 """
 
 import crypt
-import gmpy2
 import hashlib
 
 import six
@@ -15,9 +14,9 @@ import binascii
 class Srp:
     def __init__(self):
         # generator as defined by 3072bit group of RFC 5054
-        self.g = gmpy2.mpz(int(six.b('5'), 16))
+        self.g = int(six.b('5'), 16)
         # modulus as defined by 3072bit group of RFC 5054
-        self.n = gmpy2.mpz(int(six.b('''\
+        self.n = int(six.b('''\
 FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E08\
 8A67CC74020BBEA63B139B22514A08798E3404DDEF9519B3CD3A431B\
 302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9\
@@ -31,21 +30,21 @@ FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D\
 B3970F85A6E1E4C7ABF5AE8CDB0933D71E8C94E04A25619DCEE3D226\
 1AD2EE6BF12FFA06D98A0864D87602733EC86A64521F2B18177B200C\
 BBE117577A615D6C770988C0BAD946E208E24FA074E5AB3143DB5BFC\
-E0FD108E4B82D120A93AD2CAFFFFFFFFFFFFFFFF'''), 16))
+E0FD108E4B82D120A93AD2CAFFFFFFFFFFFFFFFF'''), 16)
         # HomeKit requires SHA-512 (See page 36)
         self.h = hashlib.sha512
 
-    def _calculate_k(self) -> gmpy2.mpz:
+    def _calculate_k(self) -> int:
         # calculate k (see https://tools.ietf.org/html/rfc5054#section-2.5.3)
         hash_instance = self.h()
         n = Srp.to_byte_array(self.n)
         g = bytearray.fromhex((383 * '00' + '05'))  # 383 * b'0' + '5'.encode()
         hash_instance.update(n)
         hash_instance.update(g)
-        k = gmpy2.mpz(int(binascii.hexlify(hash_instance.digest()), 16))
+        k = int(binascii.hexlify(hash_instance.digest()), 16)
         return k
 
-    def _calculate_u(self) -> gmpy2.mpz:
+    def _calculate_u(self) -> int:
         if self.A is None:
             raise RuntimeError('Client\'s public key is missing')
         if self.B is None:
@@ -55,7 +54,7 @@ E0FD108E4B82D120A93AD2CAFFFFFFFFFFFFFFFF'''), 16))
         B_b = Srp.to_byte_array(self.B)
         hash_instance.update(A_b)
         hash_instance.update(B_b)
-        u = gmpy2.mpz(int(binascii.hexlify(hash_instance.digest()), 16))
+        u = int(binascii.hexlify(hash_instance.digest()), 16)
         return u
 
     def get_session_key(self) -> int:
@@ -66,7 +65,7 @@ E0FD108E4B82D120A93AD2CAFFFFFFFFFFFFFFFF'''), 16))
 
     @staticmethod
     def to_byte_array(num: int) -> bytearray:
-        h = gmpy2.digits(num, 16)
+        h = '{:x}'.format(num)
         if len(h) % 2 == 1:
             h = '0' + h
         return bytearray.fromhex(h)
@@ -93,22 +92,22 @@ class SrpClient(Srp):
         self.username = username
         self.password = password
         self.salt = None
-        self.a = gmpy2.mpz(int(binascii.hexlify(crypt.mksalt(crypt.METHOD_SHA512)[3:].encode()), 16))
+        self.a = int(binascii.hexlify(crypt.mksalt(crypt.METHOD_SHA512)[3:].encode()), 16)
         self.A = pow(self.g, self.a, self.n)
         self.B = None
 
     def set_salt(self, salt):
         if isinstance(salt, bytearray):
-            self.salt = gmpy2.mpz(salt.hex(), 16)
+            self.salt = int(salt.hex(), 16)
         else:
             self.salt = salt
 
-    def get_public_key(self) -> gmpy2.mpz:
+    def get_public_key(self) -> int:
         return pow(self.g, self.a, self.n)
 
     def set_server_public_key(self, B):
         if isinstance(B, bytearray):
-            self.B = gmpy2.mpz(B.hex(), 16)
+            self.B = int(B.hex(), 16)
         else:
             self.B = B
 
@@ -156,14 +155,14 @@ class SrpClient(Srp):
 
     def verify_servers_proof(self, M):
         if isinstance(M, bytearray):
-            tmp = gmpy2.mpz(M.hex(), 16)
+            tmp = int(M.hex(), 16)
         else:
             tmp = M
         hash_instance = self.h()
         hash_instance.update(Srp.to_byte_array(self.A))
         hash_instance.update(Srp.to_byte_array(self.get_proof()))
         hash_instance.update(Srp.to_byte_array(self.get_session_key()))
-        return tmp == gmpy2.mpz(binascii.hexlify(hash_instance.digest()), 16)
+        return tmp == int(binascii.hexlify(hash_instance.digest()), 16)
 
 
 class SrpServer(Srp):
@@ -178,7 +177,7 @@ class SrpServer(Srp):
         self.verifier = self._get_verifier()
         salt = crypt.mksalt(crypt.METHOD_SHA256)[3:].encode()
         salt_b = binascii.hexlify(salt)
-        self.b = gmpy2.mpz(int(salt_b, 16))
+        self.b = int(salt_b, 16)
         k = self._calculate_k()
         g_b = pow(self.g, self.b, self.n)
         self.B = (k * self.verifier + g_b) % self.n
@@ -186,13 +185,13 @@ class SrpServer(Srp):
         self.A = None
 
     @staticmethod
-    def _create_salt() -> gmpy2.mpz:
+    def _create_salt() -> int:
         salt = crypt.mksalt(crypt.METHOD_SHA512)[3:]
         salt_b = salt.encode()
         salt_hex = binascii.hexlify(salt_b)
         salt_int = int(salt_hex, 16)
         assert len(salt) == 16
-        return gmpy2.mpz(salt_int)
+        return salt_int
 
     def _get_verifier(self) -> int:
         hash_value = self._calculate_x()
@@ -245,14 +244,14 @@ class SrpServer(Srp):
         hash_instance.update(Srp.to_byte_array(self.B))
         hash_instance.update(K)
         r = binascii.hexlify(hash_instance.digest())
-        return m == gmpy2.mpz(r, 16)
+        return m == int(r, 16)
 
-    def get_proof(self, m) -> gmpy2.mpz:
+    def get_proof(self, m) -> int:
         hash_instance = self.h()
         hash_instance.update(Srp.to_byte_array(self.A))
         hash_instance.update(Srp.to_byte_array(m))
         hash_instance.update(Srp.to_byte_array(self.get_session_key()))
-        return gmpy2.mpz(binascii.hexlify(hash_instance.digest()), 16)
+        return int(binascii.hexlify(hash_instance.digest()), 16)
 
 
 if __name__ == '__main__':
